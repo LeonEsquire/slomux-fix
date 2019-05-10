953329c82f6490828d06cb72fbb39a09249b93d6
Leon <leon@MacBook-Leon.local>
2019-05-10 10:37:06 +0300

    Fixed

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 function _extends() {
-  _extends =
+  // сюда добавил `var`, чтобы не переписывалось свойство `_extends` (при LHS look-up)
+  // на глобальном объекте `window`.
+  // Если такой код "скормим" webpack'у 
+  // (а в нем по умолчанию выставлен режим `use strict`)
+  // и данная переменная будет иметь другое название
+  // (к примеру `_extp`), тогда переменная (а точнее свойство) не будет
+  // создана на глобальном объекте и мы увидим ошибку
+  // `ReferenceError`.
+  // Почему ваш код работает:
+  // при первом выполнении функции исполняется строка `return _extends.apply(this, arguments)`,
+  // благодаря которой внешний `_extends` корректно передает поведение вызываемого внутреннего 
+  // `_extends`, а при последующих вызовах код будет работать и без этой строки
+  var _extends =
     Object.assign ||
     function(target) {
       for (var i = 1; i < arguments.length; i++) {
@@ -45,15 +57,26 @@ const createStore = (reducer, initialState) => {
     listeners.forEach(listener => listener());
   };
 
-  const subscribe = listener => listeners.push(listener);
+  const subscribe = listener => {
+    listeners.push(listener);
+
+    //  для возможной отписки получателя оповещений
+    return () => {
+      listeners = listeners.filter(lr => lr !== listener);
+    };
+  };
 
   return { getState, dispatch, subscribe };
 };
 
 const connect = (mapStateToProps, mapDispatchToProps) => Component => {
   class WrappedComponent extends React.Component {
-    constructor(...args) {
-      super(...args);
+    // нам передали объект, потом мы его поместили в массив(rest), 
+    // чтобы далее достать данный объект из массива(spread) и задействовать его
+    // в качестве аргумента `super()`. Ниже я просто исключаю две
+    // лишние операции (rest/spread)
+    constructor(props) {
+      super(props);
       _defineProperty(
         this,
         "handleChange",
@@ -69,13 +92,30 @@ const connect = (mapStateToProps, mapDispatchToProps) => Component => {
         _extends(
           {},
           this.props,
-          mapStateToProps(this.context.store.getState(), this.props),
-          mapDispatchToProps(this.context.store.dispatch, this.props)
+          // обе функции принимают по одному аргументу
+          // и для корректной работы им пропсы не нужны.
+          // функции возвращают объекты, свойства которых
+          // далее передаются новому объекту (первый аргумент `_extends`)
+          mapStateToProps(this.context.store.getState()),
+          mapDispatchToProps(this.context.store.dispatch)
         )
       );
     }
-    componentDidUpdate() {
-      this.context.store.subscribe(this.handleChange);
+
+    // `...DidUpdate`, при каждом вызове 
+    // обновления нашего компонента, будет повторно регистрировать
+    // один и тот же обработчик событий. Не говоря о том, что при 
+    // первой прорисовке, компонент вообще никак не будет подписан
+    // на изменения store'а
+    componentDidMount() {
+      // сохраняем ссылку на функцию отписки
+      this.unsubscribe = this.context.store.subscribe(this.handleChange);
+    }
+    
+    // отписываемся от оповещений, при демонтировании компонента,
+    // так как мы не сможем оповещать не существующий компонент
+    componentWillUnmount() {
+      this.unsubscribe();
     }
   }
 
@@ -103,7 +143,7 @@ Provider.childContextTypes = {
 
   // APP
 
-  // actions
+  // actionTypes - больше нра:)
 };
 const CHANGE_INTERVAL = "CHANGE_INTERVAL";
 
@@ -119,7 +159,9 @@ const reducer = (state, action) => {
     case CHANGE_INTERVAL:
       return (state += action.payload);
     default:
-      return {};
+      // получается на наш store сможет влиять абсолютно любой action.
+      // Возвращаем просто state, чтобы миновать не интересующие нас action'ы
+      return state;
   }
 };
 
@@ -142,7 +184,13 @@ class IntervalComponent extends React.Component {
         null,
         React.createElement(
           "button",
-          { onClick: () => this.props.changeInterval(-1) },
+          {
+            onClick: () => {
+              // исключение отрицательного значения счетчика
+              if (this.props.currentInterval === 0) return;
+              this.props.changeInterval(-1);
+            }
+          },
           "-"
         ),
         React.createElement(
@@ -155,22 +203,34 @@ class IntervalComponent extends React.Component {
   }
 }
 
+// mapStateToProps - передаем состояние в качестве props, 
+// а тут мы пытались передать функцию dispatch вместо
+// состояния store'a (результат вызова this.context.store.getState())
 const Interval = connect(
-  dispatch => ({
-    changeInterval: value => dispatch(changeInterval(value))
-  }),
-
   state => ({
     currentInterval: state
+  }),
+  dispatch => ({
+    changeInterval: value => {
+      dispatch(changeInterval(value));
+    }
   })
 )(IntervalComponent);
 
 class TimerComponent extends React.Component {
-  constructor(...args) {
-    super(...args);
+  // нам передали объект, потом мы его поместили в массив(rest), 
+  // чтобы далее достать данный объект из массива(spread) и задействовать его
+  // в качестве аргумента `super()`. Ниже я просто исключаю две
+  // лишние операции (rest/spread)
+  constructor(props) {
+    super(props);
     _defineProperty(this, "state", {
       currentTime: 0
     });
+
+    // чтобы не терять контекст `this`
+    this.handleStart = this.handleStart.bind(this);
+    this.handleStop = this.handleStop.bind(this);
   }
 
   render() {
@@ -204,16 +264,32 @@ class TimerComponent extends React.Component {
   }
 
   handleStart() {
-    setTimeout(
+    // исключаем запуск секундомера с интервалом в 0 секунд,
+    // а также добавляем остановку, при повторном нажатии 
+    // `старта` (со значением интервала равным 0), во время
+    // работы секундомера с отличным от 0 интервалом
+    if (this.props.currentInterval === 0) return clearInterval(this.interval);
+
+    // останавливаем секундомер перед его повторным запуском
+    if (this.interval !== "undefined") clearInterval(this.interval);
+
+    // выносим значение интервала секундомера за пределы `setState`,
+    // чтобы секундомер не менял свой интервал при каждом нажатии
+    // конопок `+/-` (т.е. без участия кнопки `start`)
+    const { currentInterval } = this.props;
+
+    this.interval = setInterval(
       () =>
-        this.setState({
-          currentTime: this.state.currentTime + this.props.currentInterval
-        }),
-      this.props.currentInterval
+        this.setState(state => ({
+          currentTime: state.currentTime + currentInterval
+        })),
+      currentInterval * 1000
     );
   }
 
   handleStop() {
+    // останавливаем секундомер
+    clearInterval(this.interval);
     this.setState({ currentTime: 0 });
   }
 }
@@ -229,7 +305,8 @@ const Timer = connect(
 ReactDOM.render(
   React.createElement(
     Provider,
-    { store: createStore(reducer) },
+    // задаем начальное значение состояния хранилища
+    { store: createStore(reducer, 0) },
     React.createElement(Timer, null)
   ),
 
